e07f866e2e24f1a023189cf27fe6a4be383d0c16
HeckCodes <chandramaulishastri@gmail.com>
2023-06-27 12:36:49 +0530

    Added Charts

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,7 +1,9 @@
 import 'package:animephilic/components/stat_card.dart';
 import 'package:animephilic/database/database_export.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({super.key});
@@ -41,27 +43,226 @@ class _AccountScreenState extends State<AccountScreen> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   const SizedBox(height: 16),
-                  Center(
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(state.user!.picture),
-                      radius: 64,
-                    ),
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Text(
-                        state.user!.name,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 18),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Chip(
+                              avatar: const Icon(FontAwesomeIcons.clock),
+                              label: Text(
+                                  "${state.user!.joined.day}.${state.user!.joined.month}.${state.user!.joined.year}"),
+                            ),
+                            Chip(
+                              avatar: const Icon(Icons.location_on_rounded),
+                              label: Text("${state.user!.location}"),
+                            ),
+                            Chip(
+                              avatar: const Icon(Icons.cake_rounded),
+                              label: Text(
+                                  "${state.user!.birthday?.day.toString().padLeft(2, '0')}.${state.user!.birthday?.month.toString().padLeft(2, '0')}.${state.user!.birthday?.year}"),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(state.user!.picture),
+                                radius: 80,
+                              ),
+                            ),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Text(
+                                  state.user!.name,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                   const Divider(),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                    child: Text(
+                      "Anime Statictics",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        height: 200,
+                        child: PieChart(
+                          PieChartData(
+                            sectionsSpace: 0,
+                            centerSpaceRadius: 0,
+                            sections: [
+                              PieChartSectionData(
+                                color: Theme.of(context).colorScheme.secondary,
+                                value:
+                                    state.stat!.numItemsPlanToWatch.toDouble(),
+                                title: '',
+                                radius: 80,
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                              PieChartSectionData(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .inversePrimary,
+                                value: state.stat!.numItemsCompleted.toDouble(),
+                                title: '',
+                                radius: 80,
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                              PieChartSectionData(
+                                color: Theme.of(context).colorScheme.error,
+                                value: state.stat!.numItemsDropped.toDouble(),
+                                title: '',
+                                radius: 80,
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                              PieChartSectionData(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .tertiaryContainer,
+                                value: state.stat!.numItemsOnHold.toDouble(),
+                                title: '',
+                                radius: 80,
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                              PieChartSectionData(
+                                color: Theme.of(context).colorScheme.primary,
+                                value: state.stat!.numItemsWatching.toDouble(),
+                                title: '',
+                                radius: 80,
+                                titlePositionPercentageOffset: 0.55,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Chip(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.primary,
+                            label: Text(
+                              "Watching: ${state.stat!.numItemsWatching}",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                            ),
+                          ),
+                          Chip(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.tertiaryContainer,
+                            label: Text(
+                              "On Hold: ${state.stat!.numItemsOnHold}",
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .onTertiaryContainer,
+                              ),
+                            ),
+                          ),
+                          Chip(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.error,
+                            label: Text(
+                              "Dropped: ${state.stat!.numItemsDropped}",
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.onError,
+                              ),
+                            ),
+                          ),
+                          Chip(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.inversePrimary,
+                            label: Text(
+                              "Completed: ${state.stat!.numItemsCompleted}",
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .onPrimaryContainer,
+                              ),
+                            ),
+                          ),
+                          Chip(
+                            backgroundColor:
+                                Theme.of(context).colorScheme.secondary,
+                            label: Text(
+                              "Plan to Watch: ${state.stat!.numItemsPlanToWatch}",
+                              style: TextStyle(
+                                color:
+                                    Theme.of(context).colorScheme.onSecondary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Tooltip(
+                        message: "Average Rating",
+                        preferBelow: false,
+                        child: Chip(
+                          avatar: const Icon(Icons.star_rounded),
+                          label: Text("${state.stat!.meanScore}"),
+                        ),
+                      ),
+                      Tooltip(
+                        message: "Episodes",
+                        preferBelow: false,
+                        child: Chip(
+                          avatar: const Icon(Icons.movie_rounded),
+                          label: Text("${state.stat!.numEpisodes}"),
+                        ),
+                      ),
+                      Tooltip(
+                        message: "Days Watched",
+                        preferBelow: false,
+                        child: Chip(
+                          avatar: const Icon(Icons.timeline_rounded),
+                          label: Text("${state.stat!.numDaysWatched}"),
+                        ),
+                      ),
+                      Tooltip(
+                        message: "Total Items",
+                        preferBelow: false,
+                        child: Chip(
+                          avatar: const Icon(Icons.numbers_rounded),
+                          label: Text("${state.stat!.numItems}"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
                   GridView.count(
                     shrinkWrap: true,
-                    crossAxisCount: 2,
-                    childAspectRatio: 2,
+                    crossAxisCount: 3,
+                    childAspectRatio: 1,
                     physics: const NeverScrollableScrollPhysics(),
                     children: [
                       StatCard(
